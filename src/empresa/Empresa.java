2be6f8d566f8887f7a206fc9e667a4a0d5c5b4a5
dylandiazc <71187104+dylandiazc@users.noreply.github.com>
2021-09-17 09:00:24 -0500

    se crea menu

--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -5,6 +5,8 @@
  */
 package empresa;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 57305
@@ -15,7 +17,25 @@ public class Empresa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Scanner entrada = new Scanner(System.in);
+        int opcion; 
+        
+        do{
+           
+           System.out.println("bienvenidos");
+           System.out.println("**************");
+           System.out.println("1. Agregar Empleados");
+           System.out.println("2. Mostrar Empleados");
+           System.out.println("3. MOstrar Empleados");
+           System.out.println("4. Salir");
+           System.out.println("");
+           
+            System.out.println("Digite una opcion");
+            opcion=entrada.nextInt();
+                
+        }while(opcion!=4);
+        
     }
     
 }
